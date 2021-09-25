fb1b492aa6c11e55c83c792f5e9eab5251a08b60
bhickey777 <90266950+bhickey777@users.noreply.github.com>
2021-09-25 10:44:43 -0500

    v1 - base main page

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyExpenseApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyExpenseApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -13,7 +13,6 @@ class MyExpenseApp extends StatelessWidget {
     return MaterialApp(
       title: 'Expenses',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: const MyExpensePage(title: 'Expense App Page'),
@@ -31,19 +30,6 @@ class MyExpensePage extends StatefulWidget {
 }
 
 class _MyExpensePageState extends State<MyExpensePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,26 +37,11 @@ class _MyExpensePageState extends State<MyExpensePage> {
         title: Text(widget.title),
       ),
       body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Text(
+          'Widget Playground',
+          style: Theme.of(context).textTheme.headline4,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
